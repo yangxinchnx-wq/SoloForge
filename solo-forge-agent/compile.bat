e2cea9306b61370e1a8549703dcfe58ec7580cc1
yangxinchnx <yangxinchnx@users.noreply.github.com>
2026-07-19 01:25:10 +0800

    chore: remove local compile.bat (contains hardcoded local paths)

--- solo-forge-agent/compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "JAVA_HOME=C:\Users\yangx\Java\jdk-23.0.1"
-set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
-cd /d c:\Users\yangx\Desktop\SoloForge\solo-forge-agent
-set "MAVEN_PROJECTBASEDIR=c:\Users\yangx\Desktop\SoloForge\solo-forge-agent"
-set "CLASSPATH=%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
-"%JAVA_EXE%" -Dmaven.multiModuleProjectDirectory="%MAVEN_PROJECTBASEDIR%" -classpath "%CLASSPATH%" org.apache.maven.wrapper.MavenWrapperMain compile -q -DskipTests
